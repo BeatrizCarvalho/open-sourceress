a6faee377740463419768fa58b20b2675dc7c839
Beatriz Martins de Carvalho <martinsdecarvalhobeatriz@gmail.com>
2021-01-15 21:40:44 +0000

    Create MenuBar, SideBar, SocialLinks  component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import Sidebar from "../Sidebar"
+import MenuBar from "../MenuBar"
 
 import * as S from "./styled"
 import GlobalStyles from "../../styles/global"
@@ -12,6 +13,7 @@ const Layout = ({ children }) => {
       <GlobalStyles />
       <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
     </S.LayoutWrapper>
   )
 }
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,22 @@
+//const handle = "johndoe"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/BeatrizCarvalho`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/OpenSourceresss`,
+  },
+  /*  {
+    label: "Youtube",
+    url: `https://www.youtube.com/${handle}`,
+  },*/
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/open.sourceress`,
+  },
+]
+
+export default links
