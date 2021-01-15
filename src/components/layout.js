41545923d94498c5590785d4a122f636b468d900
Beatriz Martins de Carvalho <martinsdecarvalhobeatriz@gmail.com>
2021-01-15 20:12:15 +0000

    Create First Component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,17 @@ import React from "react"
 import PropTypes from "prop-types"
 
 //import Header from "./header"
+import Profile from "./Profile"
 
 const Layout = ({ children }) => {
-  return <main>{children}</main>
+  return (
+    <>
+      <aside>
+        <Profile />
+      </aside>
+      <main>{children}</main>
+    </>
+  )
 }
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,20 +6,6 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <ul>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <Link to="/" activeStyle={{ color: "red" }}>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about">About (Gatsby Link)</Link>
-      </li>
-    </ul>
   </Layout>
 )
 
