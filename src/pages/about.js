ff539192e0626794c92a856fad7af16d100637d5
Beatriz Martins de Carvalho <martinsdecarvalhobeatriz@gmail.com>
2021-01-15 20:49:27 +0000

    Add styles to Layout Component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
 const AboutPage = () => (
   <>
     <h1>About Page</h1>
