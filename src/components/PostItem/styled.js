cfdea9089b91c703de16a3cce5a973c3420c5b82
BeatrizCarvalho <martinsdecarvalhobeatriz@gmail.com>
2021-01-26 17:09:00 +0000

    Do Light/Dark Theme and display method

--- src/components/PostItem/styled.js
+++ src/components/PostItem/styled.js
@@ -5,6 +5,11 @@ export const PostItemLink = styled(Link)`
   color: var(--texts);
   display: flex;
   text-decoration: none;
+
+  body#grid & {
+    background-color: var(--background);
+  }
+
   &:hover {
     color: var(--highlight);
   }
@@ -16,6 +21,12 @@ export const PostItemWrapper = styled.section`
   display: flex;
   padding: 2rem 3rem;
   width: 100%;
+  body#grid & {
+    border: none;
+    padding: 2rem 1rem;
+    flex-direction: column;
+    justify-content: center;
+  }
 `
 
 export const PostItemTag = styled.div`
@@ -31,12 +42,21 @@ export const PostItemTag = styled.div`
   min-height: 90px;
   min-width: 90px;
   text-transform: uppercase;
+
+  body#grid & {
+    margin-bottom: 1.5rem;
+  }
 `
 
 export const PostItemInfo = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 1.5rem;
+
+  body#grid & {
+    line-height: 1.1;
+    margin: 0.8rem 0;
+  }
 `
 
 export const PostItemDate = styled.time`
